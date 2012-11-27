0c7259e11ed8dddab7b63db39f9aa04dcc4d4b72
KiwiStrongis <kiwistrongis@gmail.com>
2012-11-27 00:39:12 -0500

    updating parsing

--- assets/parsing.c
+++ assets/parsing.c
@@ -1,1 +1,20 @@
 #include "parsing.h"
+
+
+int parse(char* s, inode* dest){
+	int length = strlen(s);
+	free (dest);
+	if (length <=0)
+		return -1;
+	else if ( length ==1)
+		if(s[0]=='/'){
+			dest = (inode*)&(super.root);
+			return 0;}
+		else
+			return -1;
+	else {
+		return parse_it(s+1,dest,super.root);}}
+
+int parse_it(char* s, inode* dest, inode i);/*{
+	char* dref = strchr(s,'/');
+	int length = dref-s;*/
--- assets/parsing.h
+++ assets/parsing.h
@@ -1,11 +1,12 @@
 #include "superblock.h"
 #include "../assets.h"
+#include <string.h>
 
 /**
  *the rootof the parsing algorithm
  **/
-int parse(char* s, *inode dest);
+int parse(char* s, inode* dest);
 /**
  *an iteration of the parsing algorithm
  **/
-int parse_it(char* s, *inode dest, inode i);
+int parse_it(char* s, inode* dest, inode i);
